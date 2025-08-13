99430c50d973eba75a28882622300117880e037f
pehsanchez <psanchez.souza@gmail.com>
2025-08-12 22:21:46 -0300

    Create exenplo1.c

---
+++ exenplo1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	//imprime algumas mensagens na tela
+	printf("Mackenzie - FCI\n");
+	printf("Algoritmos e ");
+	printf("Prog. II\n");
+	printf("Turma: 02P\n");
+	
+	return 0;
+}
